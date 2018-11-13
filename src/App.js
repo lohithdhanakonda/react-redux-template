011d6b6847ff84d934a8ef15bee8fe339c72a94f
Lohith.Dhanakonda <lohith.dhanakonda@ggktech.com>
2018-11-13 19:30:38 +0530

    Fix: added redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
 import './styles/app.scss';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header></Header>
+        <Sidebar></Sidebar>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middleware = [thunk]
+const store = createStore(
+    reducers,
+    applyMiddleware(...middleware)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
